16d1df5500d6a59546a1416f4a31c8ea233acd1d
Mohamed El Gindi <mo.gindi@gmail.com>
2023-08-31 08:45:42 +0000

    decompress xz images on the fly

--- scripts/openstack/upload-images.sh
+++ scripts/openstack/upload-images.sh
@@ -14,11 +14,20 @@ CONFIG_DIR=$(pwd)/etc/kolla
 # upload ubuntu image
 image_urls=(
 	https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-	https://dl.fedoraproject.org/pub/alt/atomic/stable/Fedora-Atomic-27-20180419.0/CloudImages/x86_64/images/Fedora-Atomic-27-20180419.0.x86_64.qcow2
+	https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/35.20220116.3.0/x86_64/fedora-coreos-35.20220116.3.0-openstack.x86_64.qcow2.xz
 )
 
 for image_url in ${image_urls[@]}; do
+	pipe_cmd=cat
+	# remove file extension
 	image_name=$(echo $(basename $image_url) | grep -o ".*\." | head -c -2)
+	if [[ "$image_url" == *".xz" ]]; then
+		echo Image detected to be xz compressed. Will decompress.
+		# removing file extension again - (probably .qcow2 or .img)
+		image_name=$(echo $image_name | grep -o ".*\." | head -c -2)
+		pipe_cmd="xz -d -"
+
+	fi
 	image_type=qcow2
-	openstack image show $image_name || curl $image_url --output - | openstack image create $image_name --disk-format qcow2
+	openstack image show $image_name || curl $image_url --output - | $pipe_cmd | openstack image create $image_name --disk-format qcow2
 done
