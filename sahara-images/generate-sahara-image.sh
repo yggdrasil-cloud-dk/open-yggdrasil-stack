89bc56f2c1561bbbbec0be0bc0998aa25bd95f46
Mohamed El Gindi <mo.gindi@gmail.com>
2023-09-17 11:59:54 +0000

    progress on generating sahara images

--- sahara-images/generate-sahara-image.sh
+++ sahara-images/generate-sahara-image.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-apt install -y tox qemu-utils kpartx
+set -x
+apt install -y tox qemu-utils kpartx ntp
 
-git clone --branch stable/2023.1 https://opendev.org/openstack/sahara-image-elements
+if [ ! -d "sahara-image-elements" ]; then
+    git clone --branch stable/2023.1 https://opendev.org/openstack/sahara-image-elements
+    cd sahara-image-elements
+    git apply ../sahara-image-elements.patch
+    cd ..
+fi
+
 cd sahara-image-elements
-
-git apply ../sahara-image-elements.patch
-DIB_DEBUG_TRACE=1 DIB_SPARK_VERSION=3.5.0 DIB_CDH_VERSION=5.11 SPARK_HADOOP_DL=hadoop3 tox -e venv -- sahara-image-create
+#DIB_DEBUG_TRACE=1 DIB_SPARK_VERSION=3.5.0 DIB_CDH_VERSION=5.11 SPARK_HADOOP_DL=hadoop3 PLUGIN=vanilla HADOOP_VERSION=3.0.1 tox -e venv -- sahara-image-create
+#DIB_DEBUG_TRACE=1 DIB_SPARK_VERSION=3.5.0 DIB_CDH_VERSION=5.11 SPARK_HADOOP_DL=hadoop3  HADOOP_VERSION=3.0.1 BASE_IMAGE_OS=ubuntu tox -e venv -- sahara-image-create
+DIB_DEBUG_TRACE=1 DIB_RELEASE=jammy HADOOP_VERSION=3.0.1 PLUGIN=vanilla BASE_IMAGE_OS=ubuntu tox -e venv -- sahara-image-create
