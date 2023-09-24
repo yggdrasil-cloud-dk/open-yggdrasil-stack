f750cdcb9fc815fd1911174c413e85e2cf4a6065
Mohamed El Gindi <mo.gindi@gmail.com>
2023-09-24 19:51:57 +0000

    minor fix grafana import

--- scripts/lma/grafana/import.sh
+++ scripts/lma/grafana/import.sh
@@ -5,7 +5,7 @@ set -xe
 admin_pass=$(docker exec -it  grafana cat /etc/grafana/grafana.ini | grep ^admin_password | awk '{print $3}')
 admin_pass=${admin_pass%%[[:cntrl:]]}
 
-openstack_dashboard_id=YZCsB1Qmz
+dashboard_jsonfile=$(dirname "$0")/openstack-dashboard.json
+dashboard_id=$(cat $dashboard_jsonfile | jq -r .dashboard.uid)
 
-# check if dashboard exists 
-curl -fs http://admin:$admin_pass@10.0.10.1:3000/api/dashboards/uid/$openstack_dashboard_id -o /dev/null || curl http://admin:$admin_pass@10.0.10.1:3000/api/dashboards/import -X POST -H "Content-Type: application/json" -d @$(dirname "$0")/openstack-dashboard.json
+curl -fs http://admin:$admin_pass@10.0.10.1:3000/api/dashboards/uid/$dashboard_id -o /dev/null || curl http://admin:$admin_pass@10.0.10.1:3000/api/dashboards/import -X POST -H "Content-Type: application/json" -d @$dashboard_jsonfile
