8a08807c92eb38cb89afca7567cd25a76e074340
Mohamed El Gindi <mo.gindi@gmail.com>
2023-09-28 19:19:08 +0000

    cloudkittyyyyyy

--- scripts/openstack/install-client.sh
+++ scripts/openstack/install-client.sh
@@ -9,9 +9,11 @@ cd workspace
 source kolla-venv/bin/activate
 
 # install openstack client
-pip install -U python-openstackclient -c https://releases.openstack.org/constraints/upper/2023.1
-pip install -U python-heatclient -c https://releases.openstack.org/constraints/upper/2023.1
-pip install -U python-troveclient -c https://releases.openstack.org/constraints/upper/2023.1
-pip install -U python-magnumclient -c https://releases.openstack.org/constraints/upper/2023.1
-pip install -U python-cloudkittyclient -c https://releases.openstack.org/constraints/upper/2023.1
-
+pip install -U -c https://releases.openstack.org/constraints/upper/2023.1 \
+ python-openstackclient \
+ python-heatclient \
+ python-troveclient \
+ python-magnumclient \
+ python-cloudkittyclient \
+ gnocchiclient \
+ aodhclient
--- scripts/tests/cloudkitty.sh
+++ scripts/tests/cloudkitty.sh
@@ -15,6 +15,8 @@ cloudkitty module disable pyscripts
 
 cloudkitty module set priority hashmap 100
 
+# instance up time
+
 cloudkitty hashmap group list -f value | grep -q instance_uptime_flavor_id || cloudkitty hashmap group create instance_uptime_flavor_id
 
 gid=$(cloudkitty hashmap group list -f value | grep instance_uptime_flavor_id | awk '{print $2}')
@@ -29,8 +31,30 @@ fid=$(cloudkitty hashmap field list $sid -f value | grep flavor_id | awk '{print
 
 flavor_id=$(openstack flavor show m1.tiny -f value -c id)
 
-cloudkitty hashmap mapping create 0.01 \
+cloudkitty hashmap mapping list -g $gid -f value | grep -q $flavor_id || cloudkitty hashmap mapping create 0.01 \
  --field-id $fid \
  --value $flavor_id \
  -g $gid \
  -t flat
+
+# volume per gb
+
+cloudkitty hashmap group list | grep -q volume_thresholds || cloudkitty hashmap group create volume_thresholds
+
+gid=$(cloudkitty hashmap group list -f value | grep volume_thresholds | awk '{print $2}')
+
+cloudkitty hashmap service list | grep -q volume.size || cloudkitty hashmap service create volume.size
+
+sid=$(cloudkitty hashmap service list -f value | grep volume.size | awk '{print $2}')
+
+cloudkitty hashmap mapping list -g $gid -f value | grep -q $sid || cloudkitty hashmap mapping create 0.001 \
+ -s $sid \
+ -g $gid \
+ -t flat
+
+openstack project show test || openstack project create test
+
+openstack user show test || openstack user create test --password test
+
+openstack role add --user test --project test rating
+openstack role add --user test --project test member
