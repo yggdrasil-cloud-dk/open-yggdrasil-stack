7def79e62daf1ca2adf8b6bcbf03e5070a8545d1
Mo Gindi <mo.gindi@gmail.com>
2024-02-26 00:43:21 +0100

    improve setup script

--- setup_remote_os_server.sh
+++ setup_remote_os_server.sh
@@ -2,6 +2,15 @@
 
 server=$1
 
+if [[ -z $server ]]; then
+  echo Missing Server
+  exit 1
+elif ! grep -q $server ~/.ssh/config; then
+  echo Server not found in ssh config. Exiting..
+  exit 1
+fi
+
+
 # user is only initial user to connection
 if [[ $server =~ ovh ]]; then
   user=ubuntu
@@ -10,10 +19,6 @@ elif [[ $server =~ hetzner ]]; then
 fi
 
 
-if [[ -z $user ]] || [[ -z $server ]]; then
-  echo missing vars
-  exit 1
-fi
 
 another_ssh_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2ChoqYodykOqMHA6JX4b2B9HZGdbTHpshh3Djt7dYAv1PuVNOaJSm7k65pGppRduvx5BeUhOJV73g9Jar4hHb9sbDYyG2eR0knalOqwfyOOEE2WmVy2FXjxXiC3yPDpPh5JyysQ9Y7PRjYdvjHVlV/aFSqbYrYULZQRw0UkLbObu6y/eUQfg26UmeSYax7CUn4YDWe2Nko+XzlR1gH7Vmj1DzlTMdNdRGAoC0w86OQR4gGQfnDLpoWM8k5rOuNvZHW19hF+d6l0avAfUjoGhConuevjA9KMopLmIEHTnpSXTdOsBeaVpKToIjbToNIgTZTVMOrFv+YbP/2fricElr EXT.mohamed.el.gindi@W-PC2319M5"
 
