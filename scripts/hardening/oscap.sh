04b788cf8277520bf61b71324ae6806c99399203
Mohamed El Gindi <mo.gindi@gmail.com>
2024-11-18 12:31:10 +0100

    add vulnerability scanning

---
+++ scripts/hardening/oscap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+apt -y install libopenscap8 bzip2
+
+cd /tmp 
+wget https://security-metadata.canonical.com/oval/com.ubuntu.$(lsb_release -cs).usn.oval.xml.bz2
+bzip2 -d com.ubuntu.jammy.usn.oval.xml.bz2
+
+oscap oval eval --report oval-jammy.html com.ubuntu.jammy.usn.oval.xml
+
+
