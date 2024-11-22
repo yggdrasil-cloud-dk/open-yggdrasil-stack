bed040887932bd8db98ab9f64eb74632e0adffb7
Mohamed El Gindi <mo.gindi@gmail.com>
2024-11-22 15:39:03 +0100

    fix custom metrics hang

--- scripts/lma/custom-exporter.sh
+++ scripts/lma/custom-exporter.sh
@@ -10,4 +10,22 @@ docker ps | grep -q custom_exporter || ./docker_run.sh
 
 cd ..
 
-pstree | grep -q docker.sh || (./scripts/lma/custom_metrics/docker.sh &)
+
+service=docker-custom-metrics
+cat > /etc/systemd/system/$service.service << EOF
+[Unit]
+After=docker.service
+
+[Service]
+ExecStart=-/bin/bash $(pwd)/scripts/lma/custom_metrics/docker.sh
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl daemon-reload
+systemctl restart $service
+systemctl enable $service
+
+
+exit 0
