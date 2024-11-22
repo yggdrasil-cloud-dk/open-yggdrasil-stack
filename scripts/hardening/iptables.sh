3f9e37b90fe9cb79e9fa5653046177deaafdea7c
Mohamed El Gindi <mo.gindi@gmail.com>
2024-11-22 15:46:18 +0100

    update floating ip range in iptables

--- scripts/hardening/iptables.sh
+++ scripts/hardening/iptables.sh
@@ -6,7 +6,7 @@ rules=(
   "\-A INPUT -p icmp -j ACCEPT"
   "\-A INPUT -p tcp -m tcp --dport 22 -j ACCEPT"
   "\-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT"
-  "\-A INPUT -s 192.168.1.0/24 -j ACCEPT"
+  "\-A INPUT -s 192.168.150.0/24 -j ACCEPT"
 )
 
 set -x
