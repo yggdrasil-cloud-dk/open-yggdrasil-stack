374bc9fbb529dda6242ad56b4a12b82b5978dca8
Mohamed El Gindi <mo.gindi@gmail.com>
2025-01-08 07:34:14 +0100

    add pvc migrate

--- examples/k8s-migration/migrate-pvc.sh
+++ examples/k8s-migration/migrate-pvc.sh
@@ -1,93 +1,12 @@
 #!/bin/bash
 
-SOURCE_KUBECONFIG=$SOURCE_KUBECONFIG
-SOURCE_NAMESPACE=$SOURCE_NAMESPACE
-SOURCE_PVC_NAME=$SOURCE_PVC_NAME
+set -x
 
-DEST_KUBECONFIG=$DEST_KUBECONFIG
-DEST_NAMESPACE=$DEST_NAMESPACE
-DEST_PVC_NAME=$DEST_PVC_NAME
+export SRC_KUBECONFIG=~/.kube/config-src
+export SRC_NAMESPACE=prod
+export SRC_PVC_NAME=app-data
+export DST_KUBECONFIG=~/.kube/config-dst
+export DST_NAMESPACE=prod
+export DST_PVC_NAME=app-data
 
-
-set -xe
-
-SOURCE_EXTRA_ARGS="--kubeconfig $SOURCE_KUBECONFIG -n $SOURCE_NAMESPACE"
-DEST_EXTRA_ARGS="--kubeconfig $DEST_KUBECONFIG -n $DEST_NAMESPACE"
-
-# Checks
-
-# ensure 'pv' command installed
-which pv
-
-source_pvc_used_by=$(kubectl $SOURCE_EXTRA_ARGS describe pvc $SOURCE_PVC_NAME  | grep "^Used By:" | awk '{print $3}')
-test $source_pvc_used_by == "<none>" || ( echo "source pvc used by pod(s) $source_pvc_used_by. Please ensure they are not used. Exiting.." ; exit 1 )
-dest_pvc_used_by=$(kubectl $DEST_EXTRA_ARGS describe pvc $DEST_PVC_NAME  | grep "^Used By:" | awk '{print $3}')
-test $dest_pvc_used_by == "<none>" || ( echo "dest pvc used by pod(s) $dest_pvc_used_by. Please ensure they are not used. Exiting.." ; exit 1 )
-
-container_name=ubuntu
-cat <<EOF | kubectl $SOURCE_EXTRA_ARGS apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: $container_name
-spec:
-  containers:
-  - image: ubuntu
-    command:
-      - "sleep"
-      - "infinity"
-    imagePullPolicy: IfNotPresent
-    name: ubuntu
-    volumeMounts:
-    - name: src-pvc
-      mountPath: /mnt/
-  restartPolicy: Always
-  volumes:
-  - name: src-pvc
-    persistentVolumeClaim:
-      claimName: $SOURCE_PVC_NAME
-EOF
-
-cat <<EOF | kubectl $DEST_EXTRA_ARGS apply -f -
-apiVersion: v1
-kind: Pod
-metadata:
-  name: $container_name
-spec:
-  containers:
-  - image: ubuntu
-    command:
-      - "sleep"
-      - "infinity"
-    imagePullPolicy: IfNotPresent
-    name: ubuntu
-    volumeMounts:
-    - name: dest-pvc
-      mountPath: /mnt/
-  restartPolicy: Always
-  volumes:
-  - name: dest-pvc
-    persistentVolumeClaim:
-      claimName: $DEST_PVC_NAME
-EOF
-
-# wait for pods to be ready
-while kubectl $SOURCE_EXTRA_ARGS get pods $container_name --output="jsonpath={.status.containerStatuses[*].ready}" | grep false; do sleep 5; done
-while kubectl $DEST_EXTRA_ARGS get pods $container_name --output="jsonpath={.status.containerStatuses[*].ready}" | grep false; do sleep 5; done
-
-# prepare pods
-STARTUP_COMMANDS="echo no pre-migration commands"
-( kubectl $SOURCE_EXTRA_ARGS exec $container_name -- /bin/bash -c "$STARTUP_COMMANDS" ) &
-( kubectl $DEST_EXTRA_ARGS exec $container_name -- /bin/bash -c "$STARTUP_COMMANDS" ) &
-wait
-
-# clean destination files (just incase)
-kubectl $DEST_EXTRA_ARGS exec -i $container_name -- /bin/bash -c 'set -x; cd /mnt; echo Deleteing the following: ; ls -a | tail -n +3 | xargs rm -rf '
-
-# copy with tar
-kubectl $SOURCE_EXTRA_ARGS exec -i $container_name -- /bin/bash -c 'tar czf - /mnt/' | pv | kubectl $DEST_EXTRA_ARGS exec -i $container_name -- /bin/bash -c 'tar xzf - -C /'
-
-# delete pods
-( kubectl $SOURCE_EXTRA_ARGS delete pod $container_name ) &
-( kubectl $DEST_EXTRA_ARGS delete pod $container_name ) &
-wait
+curl -s https://raw.githubusercontent.com/mogindi/easy-migrate-pvc/main/easy-migrate-pvc.sh | bash
