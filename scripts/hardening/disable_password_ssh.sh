1780002eb14391b94d15f41dbe83f45a39115aed
Mo G <mo.gindi@gmail.com>
2025-04-25 12:52:29 +0200

    add disable ssh password script

---
+++ scripts/hardening/disable_password_ssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find /etc/ssh/sshd_config* -type f | xargs sed -i 's/.*PasswordAuthentication yes/PasswordAuthentication no/g'
+rm -f /etc/ssh/sshd_config.d/50-cloud-init.conf
+systemctl restart sshd
