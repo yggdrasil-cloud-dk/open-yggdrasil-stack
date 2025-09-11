3b7ba15af57de763a85c1d9ec7756a758b13c803
Mohamed El Gindi <mo.gindi@gmail.com>
2025-09-11 13:38:07 +0200

    make cert creation non-interactive

--- scripts/certs.sh
+++ scripts/certs.sh
@@ -5,6 +5,6 @@ set -xe
 apt install -y certbot
 
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-certbot certonly --standalone -d console.yggdrasilcloud.dk --staple-ocsp -m mo.gindi@gmail.com --agree-tos
+certbot certonly --non-interactive --standalone -d console.yggdrasilcloud.dk --staple-ocsp -m mo.gindi@gmail.com --agree-tos
 while iptables -D INPUT -p tcp --dport 80 -j ACCEPT; do echo removed rule; done || true
 
