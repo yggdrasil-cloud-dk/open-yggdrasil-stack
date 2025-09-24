91893e48fdade3c599fe8eca41732f9c815ac592
Mohamed El Gindi <mo.gindi@gmail.com>
2025-09-24 16:24:39 +0200

    update port 8443 in iptables

--- scripts/hardening/iptables.sh
+++ scripts/hardening/iptables.sh
@@ -17,7 +17,7 @@ rules=(
   "\-A INPUT -s $NETWORK_PROVIDER_NET -j ACCEPT"
   "\-A INPUT -s $NODE_MGMT_NET -j ACCEPT"
   "\-A INPUT -p tcp --dport 443 -m state --state NEW --syn -m hashlimit --hashlimit 15/s --hashlimit-burst 30 --hashlimit-mode srcip --hashlimit-srcmask 32 --hashlimit-name synattack -j ACCEPT"
-  "\-A INPUT -p tcp --dport 8443 -m state --state NEW --syn -m hashlimit --hashlimit 15/s --hashlimit-burst 30 --hashlimit-mode srcip --hashlimit-srcmask 32 --hashlimit-name synattack -j ACCEPT"
+  "\-A INPUT -p tcp --dport 8443 -j ACCEPT"
 )
 
 set -x
